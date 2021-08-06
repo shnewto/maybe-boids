ac1424ac98102690b9ddeb93b708d150b919c48d
Shea Newton <shnewto@gmail.com>
2021-08-05 20:14:43 -0600

    experiment with collisions and the size of the map

--- src/boids.c
+++ src/boids.c
@@ -1,101 +1,100 @@
 #include "raylib.h"
 
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
+int main(void) {
     const int screen_width = 800;
     const int screen_height = 450;
-
+    int world_size = 35;
+    float resolution = 1.0f;
+    float bounds = world_size * resolution / 2;
     InitWindow(screen_width, screen_height, "boid collisions");
 
-    // Define the camera to look into our 3d world
-    Camera camera = { { 0.0f, 10.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0 };
-
-    Vector3 boid_pos = { 0.0f, 1.0f, 2.0f };
+    Camera camera = {{0.0f, 20.0f, 40.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, 45.0f, 0};
+    Vector3 boid_pos = {0.0f, 1.0f, 2.0f};
     float boid_size = .2f;
     Color boid_color = GREEN;
 
-    Vector3 obj_1_pos = { -4.0f, 1.0f, 0.0f };
-    Vector3 obj_1_size = { 1.0f, 2.0f, 1.0f };
+    Vector3 obj_1_pos = {-2.0f, 1.0f, -3.0f};
+    Vector3 obj_1_size = {1.0f, 2.0f, 1.0f};
 
-    Vector3 obj_2_pos = { 4.0f, 0.0f, 0.0f };
-    Vector3 obj_2_size = { 1.0f, 2.0f, 1.0f };
+    Vector3 obj_2_pos = {2.0f, 1.0f, 2.0f};
+    Vector3 obj_2_size = {1.0f, 2.0f, 1.0f};
 
     bool collision = false;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    SetTargetFPS(60);
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
+    while (!WindowShouldClose()) {
+        Vector3 maybe_boid_pos = {boid_pos.x, boid_pos.y, boid_pos.z};
 
-        // Move player
-        if (IsKeyDown(KEY_RIGHT)) boid_pos.x += 0.2f;
-        else if (IsKeyDown(KEY_LEFT)) boid_pos.x -= 0.2f;
-        else if (IsKeyDown(KEY_DOWN)) boid_pos.z += 0.2f;
-        else if (IsKeyDown(KEY_UP)) boid_pos.z -= 0.2f;
+        if (IsKeyDown(KEY_RIGHT)) {
+            maybe_boid_pos.x += 0.2f;
+        } else if (IsKeyDown(KEY_LEFT)) {
+            maybe_boid_pos.x -= 0.2f;
+        } else if (IsKeyDown(KEY_DOWN)
+                ) {
+            maybe_boid_pos.z += 0.2f;
+        } else if (
+                IsKeyDown(KEY_UP)
+                ) {
+            maybe_boid_pos.z -= 0.2f;
+        }
 
         collision = false;
 
-        // Check collisions player vs enemy-box
         if (CheckCollisionBoxSphere(
-            (BoundingBox){(Vector3){ obj_1_pos.x - obj_1_size.x/2,
-                                     obj_1_pos.y - obj_1_size.y/2,
-                                     obj_1_pos.z - obj_1_size.z/2 },
-                          (Vector3){ obj_1_pos.x + obj_1_size.x/2,
-                                     obj_1_pos.y + obj_1_size.y/2,
-                                     obj_1_pos.z + obj_1_size.z/2 }},
-            boid_pos, boid_size)) collision = true;
+                (BoundingBox) {(Vector3) {obj_1_pos.x - obj_1_size.x / 2,
+                                          obj_1_pos.y - obj_1_size.y / 2,
+                                          obj_1_pos.z - obj_1_size.z / 2},
+                               (Vector3) {obj_1_pos.x + obj_1_size.x / 2,
+                                          obj_1_pos.y + obj_1_size.y / 2,
+                                          obj_1_pos.z + obj_1_size.z / 2}},
+                maybe_boid_pos, boid_size))
+            collision = true;
 
-        // Check collisions player vs enemy-sphere
         if (CheckCollisionBoxSphere(
-            (BoundingBox){(Vector3){ obj_1_pos.x - obj_2_size.x/2,
-                                     obj_1_pos.y - obj_2_size.y/2,
-                                     obj_1_pos.z - obj_2_size.z/2 },
-                          (Vector3){ obj_1_pos.x + obj_2_size.x/2,
-                                     obj_1_pos.y + obj_2_size.y/2,
-                                     obj_1_pos.z + obj_2_size.z/2 }},
-            boid_pos, boid_size)) collision = true;
+                (BoundingBox) {(Vector3) {obj_2_pos.x - obj_2_size.x / 2,
+                                          obj_2_pos.y - obj_2_size.y / 2,
+                                          obj_2_pos.z - obj_2_size.z / 2},
+                               (Vector3) {obj_2_pos.x + obj_2_size.x / 2,
+                                          obj_2_pos.y + obj_2_size.y / 2,
+                                          obj_2_pos.z + obj_2_size.z / 2}},
+                maybe_boid_pos, boid_size))
+            collision = true;
 
-        if (collision) boid_color = RED;
-        else boid_color = GREEN;
-        //----------------------------------------------------------------------------------
+        if (maybe_boid_pos.x > bounds || maybe_boid_pos.x < -bounds ||
+        maybe_boid_pos.z > bounds || maybe_boid_pos.z < -bounds) {
+            collision = true;
+        }
 
-        // Draw
-        //----------------------------------------------------------------------------------
+        if (!collision) {
+            boid_pos.x = maybe_boid_pos.x;
+            boid_pos.y = maybe_boid_pos.y;
+            boid_pos.z = maybe_boid_pos.z;
+        }
+
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+        ClearBackground(RAYWHITE);
 
-            BeginMode3D(camera);
+        BeginMode3D(camera);
 
-                // Draw enemy-box
-                DrawCube(obj_1_pos, obj_1_size.x, obj_1_size.y, obj_1_size.z, GRAY);
-                DrawCube(obj_2_pos, obj_2_size.x, obj_2_size.y, obj_2_size.z, GRAY);
+        DrawCube(obj_1_pos, obj_1_size.x, obj_1_size.y, obj_1_size.z, GRAY);
+        DrawCube(obj_2_pos, obj_2_size.x, obj_2_size.y, obj_2_size.z, GRAY);
 
-                // Draw enemy-sphere
-                DrawSphere(boid_pos, boid_size, boid_color);
+        DrawSphere(boid_pos, boid_size, boid_color);
 
-                DrawGrid(10, 1.0f);        // Draw a grid
+        DrawGrid(world_size, resolution);
 
-            EndMode3D();
+        EndMode3D();
 
-            DrawText("Move boid with arrows to collide", 220, 40, 20, GRAY);
+//        DrawText("← ↑ ↓ →", 220, 40, 20, GRAY);
 
-            DrawFPS(10, 10);
+        DrawFPS(10, 10);
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow();
 
     return 0;
 }
